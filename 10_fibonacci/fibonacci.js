cbd9c46d6763c5c7b4575f870ccde01cf9754a73
Egor <egor.dmitriev99@gmail.com>
2023-05-05 19:13:54 +0200

    the rest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0){
+        return 'OOPS'
+    }
+    else {
+        let farr = []
+        for (let i = 0; i < a; i++){
+            if (i==0 || i==1){
+                farr[i]=1
+            }
+            else{
+                farr[i]= farr[i-1] + farr[i-2]
+            }
+        }
+        return farr.pop()
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    initValue = a.pop()
 
+    return a.reduce((accumulator, current) => {
+        if (current.yearOfDeath == undefined){
+            let date = new Date()
+            if (accumulator.yearOfDeath - accumulator.yearOfBirth <= date.getFullYear() - current.yearOfBirth){
+                return current
+            }
+            else {
+                return accumulator
+            }
+        }
+        else{
+        if (accumulator.yearOfDeath - accumulator.yearOfBirth < current.yearOfDeath - current.yearOfBirth){
+            return current
+        }
+        else {
+            return accumulator
+        }
+    }   
+    }
+    , initValue)
 };
 
 // Do not edit below this line
